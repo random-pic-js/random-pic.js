3928a7cef08c81ef99fd3c6ef15f3b8c7369e2de
hadi <65118892+hadiazt@users.noreply.github.com>
2021-08-15 14:30:19 +0430

    Better Sorting

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const pic = require('./random-pic.js')
+const pic = require('./src/random-pic.js') // With local files
+// const pic = require('random-pic.js') // npm install random-pic.js
 
-pic.help((data) => console.log(data))
 
 
 
 
 pic.animal((data) => console.log(data))
-pic.anime((data) => console.log(data))
-pic.boy((data) => console.log(data))
-pic.couple((data) => console.log(data))
-pic.eboy((data) => console.log(data))
-pic.egirl((data) => console.log(data))
-pic.girl((data) => console.log(data))
-pic.landscape((data) => console.log(data))
+// pic.anime((data) => console.log(data))
+// pic.boy((data) => console.log(data))
+// pic.couple((data) => console.log(data))
+// pic.eboy((data) => console.log(data))
+// pic.egirl((data) => console.log(data))
+// pic.girl((data) => console.log(data))
+// pic.landscape((data) => console.log(data))
 
 
--- src/random-pic.js
+++ src/random-pic.js
@@ -1,60 +1,53 @@
 const fetch = require('node-fetch');
 
-const help = async function request() {
-    const res = await fetch('https://api.hadiazari.repl.co');
-    const data = await res.json();
-    console.log(data.urls[Math.floor(Math.random() * data.urls.length)])
-}
-
-const animal = async function request() {
+const animal = async function () {
     const res = await fetch('https://api.hadiazari.repl.co/animal.json');
     const data = await res.json();
     console.log(data.animal[Math.floor(Math.random() * data.animal.length)])
 }
 
-const anime = async function request() {
+const anime = async function () {
     const res = await fetch('https://api.hadiazari.repl.co/anime.json');
     const data = await res.json();
     console.log(data.anime[Math.floor(Math.random() * data.anime.length)])
 }
 
-const boy = async function request() {
+const boy = async function () {
     const res = await fetch('https://api.hadiazari.repl.co/boy.json');
     const data = await res.json();
     console.log(data.boy[Math.floor(Math.random() * data.boy.length)])
 }
 
-const girl = async function request() {
+const girl = async function () {
     const res = await fetch('https://api.hadiazari.repl.co/girl.json');
     const data = await res.json();
     console.log(data.girl[Math.floor(Math.random() * data.girl.length)])
 }
 
-const couple = async function request() {
+const couple = async function () {
     const res = await fetch('https://api.hadiazari.repl.co/couple.json');
     const data = await res.json();
     console.log(data.couple[Math.floor(Math.random() * data.couple.length)])
 }
 
-const eboy = async function request() {
+const eboy = async function () {
     const res = await fetch('https://api.hadiazari.repl.co/eboy.json');
     const data = await res.json();
     console.log(data.eboy[Math.floor(Math.random() * data.eboy.length)])
 }
 
-const egirl = async function request() {
+const egirl = async function () {
     const res = await fetch('https://api.hadiazari.repl.co/egirl.json');
     const data = await res.json();
     console.log(data.egirl[Math.floor(Math.random() * data.egirl.length)])
 }
 
-const landscape = async function request() {
+const landscape = async function () {
     const res = await fetch('https://api.hadiazari.repl.co/landscape.json');
     const data = await res.json();
     console.log(data.landscape[Math.floor(Math.random() * data.landscape.length)])
 }
 
-exports.help = help;
 exports.animal = animal;
 exports.anime = anime;
 exports.boy = boy;
