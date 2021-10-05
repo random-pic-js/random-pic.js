5003b5dfc77baa4f49af4ac9f4e97941e163d5d8
EhsanFox <ehsan8184@gmail.com>
2021-10-05 13:58:56 +0330

    Whole Re-Write

--- src/random-pic.js
+++ src/random-pic.js
@@ -1,101 +1,148 @@
-const axios = require('axios')
+/**
+ * Random Picture Module
+ * @description Get a Random picture for any topic
+ * @module random-pic.js
+ */
 
-const animal =  async function () {
-    try {
-      const {data:response} = await axios.get('https://api.hadiazari.repl.co/animal') 
-      return response.animal[Math.floor(Math.random() * response.animal.length)]
-    }
-    catch (error) {
-      console.log(error);
-    }
-  }
+// EhsanFox was here :)
 
-  const anime =  async function () {
-    try {
-      const {data:response} = await axios.get('https://api.hadiazari.repl.co/anime') 
-      return response.anime[Math.floor(Math.random() * response.anime.length)]
-    }
-    catch (error) {
-      console.log(error);
-    }
-  }
+/**
+ * Required Packages/Modules
+ */
+const axios = require('axios');
+const { BaseURL, Endpoints } = require('./Config');
+
+/**
+ * Get a Random Animal Picture
+ * @method
+ * @returns {Promise<String>}
+ */
+exports.animal = () => {
+  return new Promise((resolve, reject) => {
+    axios.get(BaseURL + Endpoints.Animal)
+    .then(data => {
+      resolve(data.animal[Math.floor(Math.random() * response.animal.length)])
+    })
+    .catch(reject);
+  });
+}
+
+/**
+ * Get a Random Anime Picture
+ * @method
+ * @returns {Promise<String>}
+ */
+exports.anime = () => {
+  return new Promise((resolve, reject) => {
+    axios.get(BaseURL + Endpoints.Anime)
+    .then(data => {
+      resolve(data.anime[Math.floor(Math.random() * response.anime.length)])
+    })
+    .catch(reject);
+  });
+};
+
+/**
+ * Get a Random Boy Picture
+ * @method
+ * @returns {Promise<String>}
+ */
+exports.boy = () => {
+  return new Promise((resolve, reject) => {
+    axios.get(BaseURL + Endpoints.Boy)
+    .then(data => {
+      resolve(data.boy[Math.floor(Math.random() * response.boy.length)])
+    })
+    .catch(reject);
+  });
+};
   
-  const boy =  async function () {
-    try {
-      const {data:response} = await axios.get('https://api.hadiazari.repl.co/boy') 
-      return response.boy[Math.floor(Math.random() * response.boy.length)]
-    }
-    catch (error) {
-      console.log(error);
-    }
-  }
+/**
+ * Get a Random Girl Picture
+ * @method
+ * @returns {Promise<String>}
+ */
+ exports.girl = () => {
+  return new Promise((resolve, reject) => {
+    axios.get(BaseURL + Endpoints.Girl)
+    .then(data => {
+      resolve(data.girl[Math.floor(Math.random() * response.girl.length)])
+    })
+    .catch(reject);
+  });
+};
 
-  const girl =  async function () {
-    try {
-      const {data:response} = await axios.get('https://api.hadiazari.repl.co/girl') 
-      return response.girl[Math.floor(Math.random() * response.girl.length)]
-    }
-    catch (error) {
-      console.log(error);
-    }
-  }
+/**
+ * Get a Random Couple Picture
+ * @method
+ * @returns {Promise<String>}
+ */
+ exports.couple = () => {
+  return new Promise((resolve, reject) => {
+    axios.get(BaseURL + Endpoints.Couple)
+    .then(data => {
+      resolve(data.couple[Math.floor(Math.random() * response.couple.length)])
+    })
+    .catch(reject);
+  });
+};
 
-  const couple =  async function () {
-    try {
-      const {data:response} = await axios.get('https://api.hadiazari.repl.co/couple') 
-      return response.couple[Math.floor(Math.random() * response.couple.length)]
-    }
-    catch (error) {
-      console.log(error);
-    }
-  }
+/**
+ * Get a Random eBoy Picture
+ * @method
+ * @returns {Promise<String>}
+ */
+ exports.eboy = () => {
+  return new Promise((resolve, reject) => {
+    axios.get(BaseURL + Endpoints.eBoy)
+    .then(data => {
+      resolve(data.eboy[Math.floor(Math.random() * response.eboy.length)])
+    })
+    .catch(reject);
+  });
+};
 
-  const eboy =  async function () {
-    try {
-      const {data:response} = await axios.get('https://api.hadiazari.repl.co/eboy') 
-      return response.eboy[Math.floor(Math.random() * response.eboy.length)]
-    }
-    catch (error) {
-      console.log(error);
-    }
-  }
+/**
+ * Get a Random eGirl Picture
+ * @method
+ * @returns {Promise<String>}
+ */
+ exports.egirl = () => {
+  return new Promise((resolve, reject) => {
+    axios.get(BaseURL + Endpoints.eGirl)
+    .then(data => {
+      resolve(data.egirl[Math.floor(Math.random() * response.egirl.length)])
+    })
+    .catch(reject);
+  });
+};
 
-  const egirl =  async function () {
-    try {
-      const {data:response} = await axios.get('https://api.hadiazari.repl.co/egirl') 
-      return response.egirl[Math.floor(Math.random() * response.egirl.length)]
-    }
-    catch (error) {
-      console.log(error);
-    }
-  }
+/**
+ * Get a Random Landscape Picture
+ * @method
+ * @returns {Promise<String>}
+ */
+ exports.landscape = () => {
+  return new Promise((resolve, reject) => {
+    axios.get(BaseURL + Endpoints.Landscape)
+    .then(data => {
+      resolve(data.landscape[Math.floor(Math.random() * response.landscape.length)])
+    })
+    .catch(reject);
+  });
+};
 
-  const landscape =  async function () {
-    try {
-      const {data:response} = await axios.get('https://api.hadiazari.repl.co/landscape') 
-      return response.landscape[Math.floor(Math.random() * response.landscape.length)]
-    }
-    catch (error) {
-      console.log(error);
-    }
-  }
-
-  const random =  async function () {
-    try {
-      const {data:response} = await axios.get('https://api.hadiazari.repl.co/random') 
-      return response.random[Math.floor(Math.random() * response.random.length)]
-    }
-    catch (error) {
-      console.log(error);
-    }
-  } 
-
-exports.animal = animal;
-exports.anime = anime;
-exports.boy = boy;
-exports.girl = girl;
-exports.couple = couple;
-exports.eboy = eboy;
-exports.egirl = egirl;
-exports.landscape = landscape;
-exports.random = random;
+/**
+ * Get a Random Picture
+ * @method
+ * @returns {Promise<String>}
+ */
+ exports.random = () => {
+  return new Promise((resolve, reject) => {
+    axios.get(BaseURL + Endpoints.Random)
+    .then(data => {
+      resolve(data.random[Math.floor(Math.random() * response.random.length)])
+    })
+    .catch(reject);
+  });
+};
